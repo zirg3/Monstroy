418c465efcd378a3538ffa1c51c62cc4243cdd33
zirg3 <zirg31@gamil.com>
2019-08-31 01:27:53 +0300

    Шапка

--- source/js/script.js
+++ source/js/script.js
@@ -1,2 +1,13 @@
 (function () {
 })();
+
+(function () {
+  const burger = document.querySelector('.page-header__burger');
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('page-header__burger-active');
+  });
+  const cont = document.querySelector('.page-header__list');
+  burger.addEventListener('click', () => {
+    cont.classList.toggle('page-header__nav-active');
+  });
+}());
