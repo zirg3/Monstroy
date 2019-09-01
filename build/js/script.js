7604275a066cec043dd1504273825256f73f6993
zirg3 <zirg31@gamil.com>
2019-09-02 00:37:28 +0300

    Услуги

--- build/js/script.js
+++ build/js/script.js
@@ -1,13 +1,29 @@
 (function () {
+  var isMobile = false;
+  $(document).ready(function () {
+    if ($('body').width() <= 1200) {
+      isMobile = true;
+    }
+    if (isMobile) {
+      $(document).ready(function () {
+        $('.services__title').click(function () {
+          $('.services__items').removeClass('services__items--active');
+          $(this).next().toggleClass('services__items--active');
+          $('.services__title').removeClass('services__items-active');
+          $(this).toggleClass('services__items-active');
+        });
+      });
+    }
+  });
 })();
 
 (function () {
   const burger = document.querySelector('.page-header__burger');
-  burger.addEventListener('click', () => {
+  burger.addEventListener('click', function() {
     burger.classList.toggle('page-header__burger-active');
   });
   const cont = document.querySelector('.page-header__list');
-  burger.addEventListener('click', () => {
+  burger.addEventListener('click', function() {
     cont.classList.toggle('page-header__nav-active');
   });
 }());
